05f5e39b90fe0070bdc9aa8288e9dfa037c0b198
Hasebe Masahiko <JCA03205@nifty.com>
2015-03-02 21:55:06 +0900

    Initial Commit

---
+++ SoundGenerator_forMF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundGenerator_forMF
+//
+//  Created by 長谷部 雅彦 on 2015/03/02.
+//  Copyright (c) 2015年 長谷部 雅彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ SoundGenerator_forMFTests/SoundGenerator_forMFTests.swift
@@ -0,0 +1,36 @@
+//
+//  SoundGenerator_forMFTests.swift
+//  SoundGenerator_forMFTests
+//
+//  Created by 長谷部 雅彦 on 2015/03/02.
+//  Copyright (c) 2015年 長谷部 雅彦. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SoundGenerator_forMFTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
