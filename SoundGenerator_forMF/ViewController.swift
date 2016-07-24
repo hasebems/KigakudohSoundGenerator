e8dc09694d7ef5ae3bae6cbcaf30d8fa6397b32a
Hasebe Masahiko <JCA03205@gmail.com>
2016-07-24 18:44:32 +0900

    Appleへ提出

--- SoundGenerator_forMF/ViewController.swift
+++ SoundGenerator_forMF/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 	let MAX_OCT_NUM:Int8 = 3
 	let MIN_OCT_NUM:Int8 = -3
 	let tOctName = [ "-3","-2","-1","0","+1","+2","+3" ]
+	let Hole2Note:[UInt8] = [ 0, 0x4f, 0x4d, 0x51, 0x4c, 0x53, 0x4a, 0x48 ]
 	
 	//------------------------------------------------------------
 	//				Variables
@@ -27,14 +28,16 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 	var aout:SoundController? = nil
 	var transposeValue:Int8 = 0
 	var octaveNum:Int8 = 0
+	var fingerState:UInt8 = 0
+	var currentNoteNumber:UInt8 = 0
 
 	//@IBOutlet weak var textArea: UILabel!
-	@IBOutlet var midiDisplay: [UITextView]!
+//	@IBOutlet var midiDisplay: [UITextView]!
 	@IBOutlet var transposeKeyName: [UILabel]!
 	@IBOutlet var octaveNumber: [UILabel]!
 	@IBOutlet weak var vce1: UIButton!
 	@IBOutlet weak var vce2: UIButton!
-	@IBOutlet weak var midiDispEnable: UISwitch!
+//	@IBOutlet weak var midiDispEnable: UISwitch!
 	
 	@IBOutlet weak var smView: HSBScrollViewWithTouch!
 	var smusic: HSBSheetmusic!
@@ -101,25 +104,25 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 	//			Display MIDI Message
 	//------------------------------------------------------------
 	func displayMidiInfo(msg:NSString) {
-
-		if !midiDispEnable.on { return }
+		
+//		if !midiDispEnable.on { return }
 		
 		//	count how much letters are
-		var strSize = 0
-		if let mdstr:NSString = midiDisplay[0].text {
-			strSize = mdstr.length
-			if strSize > 1000 {
+//		var strSize = 0
+//		if let mdstr:NSString = midiDisplay[0].text {
+//			strSize = mdstr.length
+//			if strSize > 1000 {
 				//	clear text
-				midiDisplay[0].text = ""
-			}
-		}
+//				midiDisplay[0].text = ""
+//			}
+//		}
 
 		//	Display String
-		midiDisplay[0].replaceRange( midiDisplay[0].selectedTextRange!, withText:msg as String )
+//		midiDisplay[0].replaceRange( midiDisplay[0].selectedTextRange!, withText:msg as String )
 
 		//	Scroll to new line
-		let scrollPosition:CGPoint = CGPointMake(0, midiDisplay[0].contentSize.height)
-		midiDisplay[0].setContentOffset( scrollPosition, animated:false )
+//		let scrollPosition:CGPoint = CGPointMake(0, midiDisplay[0].contentSize.height)
+//		midiDisplay[0].setContentOffset( scrollPosition, animated:false )
 	}
 
 	
@@ -137,17 +140,6 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 		smusic.ONE_VIEW_WIDTH,smusic.ONE_VIEW_HEIGHT), animated: false)
 		smusic.currentViewNum = tKeyPage[key]*2
 	}
-	
-	//------------------------------------------------------------
-	//
-	//------------------------------------------------------------
-	func sendNoteShift(){
-		if let ao = aout {
-			ao.transpose(transposeValue+octaveNum*12)
-		}
-		changeDisplayToAppropriateKey()
-	}
-
 	//------------------------------------------------------------
 	//			Mute
 	//------------------------------------------------------------
@@ -158,18 +150,51 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 		smusic.allNoteClear()
 	}
 	//------------------------------------------------------------
-	//			Transpose
+	//			Note Shift
 	//------------------------------------------------------------
-	func setTranspose() {
+	func setNoteShift() {
 		if transposeValue > MAX_TRANSPOSE_NUM {
 			transposeValue = MIN_TRANSPOSE_NUM + transposeValue - (MAX_TRANSPOSE_NUM+1)
 		}
 		else if transposeValue < MIN_TRANSPOSE_NUM {
 			transposeValue = MAX_TRANSPOSE_NUM - (MIN_TRANSPOSE_NUM - (transposeValue+1))
 		}
+
+		let noteShift:Int8 = transposeValue + octaveNum*12
 		mute()
-		sendNoteShift()
+
+		if let ao = aout {
+			ao.transpose(noteShift)
+		}
+
+		//	only for Transpose
+		changeDisplayToAppropriateKey()
 		transposeKeyName[0].text = tKeyName[numericCast(transposeValue-MIN_TRANSPOSE_NUM)]
+	}
+	//------------------------------------------------------------
+	//			Generate Button Note Event
+	//------------------------------------------------------------
+	func generateButtonNoteEvent() {
+		var noteOrg:Int8 = Int8(Hole2Note[Int(fingerState)]) + octaveNum*12
+		while noteOrg < 0 {
+			noteOrg += 12
+		}
+		let note:UInt8 = UInt8(noteOrg)
+		if note != 0 {
+			if let ao = aout {
+				ao.noteOn(note)
+			}
+			smusic.midiNoteOn(Int(note))
+		}
+		
+		if currentNoteNumber != 0 {
+			if let ao = aout {
+				ao.noteOff(currentNoteNumber)
+			}
+			smusic.midiNoteOff(Int(currentNoteNumber))
+		}
+		
+		currentNoteNumber = note
 	}
 	//------------------------------------------------------------
 	//				Get Switch Event
@@ -183,6 +208,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 					vce1.setTitleColor(UIColor(red:1.0,green:0.0,blue:0.0,alpha:1.0), forState:UIControlState.Normal)
 					vce2.setTitleColor(UIColor(red:1.0,green:1.0,blue:1.0,alpha:1.0), forState:UIControlState.Normal)
 				}
+				fingerState = 0
+				currentNoteNumber = 0
 			case 1:
 				displayMidiInfo("PC 01h\n")
 				if let ao = aout {
@@ -190,37 +217,61 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 					vce1.setTitleColor(UIColor(red:1.0,green:1.0,blue:1.0,alpha:1.0), forState:UIControlState.Normal)
 					vce2.setTitleColor(UIColor(red:1.0,green:0.0,blue:0.0,alpha:1.0), forState:UIControlState.Normal)
 				}
+				fingerState = 0
+				currentNoteNumber = 0
 			case 2:
 				displayMidiInfo("Reset\n")
 				mute()
+				fingerState = 0
+				currentNoteNumber = 0
 			case 3:
 				transposeValue += 1
-				setTranspose()
+				setNoteShift()
 			case 4:
 				transposeValue -= 1
-				setTranspose()
+				setNoteShift()
 			case 5:
-				if ++octaveNum > MAX_OCT_NUM {
+				octaveNum += 1
+				if octaveNum > MAX_OCT_NUM {
 					octaveNum = MAX_OCT_NUM
 				}
-				sendNoteShift()
+				setNoteShift()
 				octaveNumber[0].text = tOctName[numericCast(octaveNum-MIN_OCT_NUM)]
 			case 6:
-				if --octaveNum < MIN_OCT_NUM {
+				octaveNum -= 1
+				if octaveNum < MIN_OCT_NUM {
 					octaveNum = MIN_OCT_NUM
 				}
-				sendNoteShift()
+				setNoteShift()
 				octaveNumber[0].text = tOctName[numericCast(octaveNum-MIN_OCT_NUM)]
-			case 7:
-				transposeValue += 3
-				setTranspose()
-			case 8:
-				transposeValue += 5
-				setTranspose()
-			default:
-				break
+			case 10:
+				fingerState |= 0x04
+				generateButtonNoteEvent()
+			case 11:
+				fingerState |= 0x02
+				generateButtonNoteEvent()
+			case 12:
+				fingerState |= 0x01
+				generateButtonNoteEvent()
+			default: break
 		}
 	}
-
+	//------------------------------------------------------------
+	//			Get Sw Off Event
+	//------------------------------------------------------------
+	@IBAction func releaseButton(sender: UIButton) {
+		switch sender.tag {
+		case 10:
+			fingerState &= 0xfb
+			generateButtonNoteEvent()
+		case 11:
+			fingerState &= 0xfd
+			generateButtonNoteEvent()
+		case 12:
+			fingerState &= 0xfe
+			generateButtonNoteEvent()
+		default: break
+		}
+	}
 }
 
